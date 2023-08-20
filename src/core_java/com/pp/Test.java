ede3bdeb258745f7f0be905fb4edf5523c07b9dd
Prave <Prave@LAPTOP-TMMCUANB>
2023-08-21 01:07:28 +0530

    adding Test class

---
+++ src/core_java/com/pp/Test.java
@@ -0,0 +1,12 @@
+package core_java.com.pp;
+
+public class Test {
+	int i = 10;
+	
+	public static void main(String[] args) {
+		Test t = new Test();
+		System.out.println("Hello World");
+		System.out.println(t.i);
+	}
+
+}
