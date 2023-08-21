6d5e3805eaa79652c9a2953dbc8d25a8463d6b98
Prave <Prave@LAPTOP-TMMCUANB>
2023-08-21 23:46:40 +0530

    Add two digit

--- src/core_java/com/pp/Test1.java
+++ src/core_java/com/pp/Test1.java
@@ -1,9 +1,19 @@
 package core_java.com.pp;
 
 public class Test1 {
+	
+	public Test1() {
+		int i = 10;
+		int j = 20;
+		int k = i +j;
+		System.out.println("Result +"+ k );
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Main Method");	
+		Test1  t = new Test1();
+		
+		System.out.println("Stop excution");
 
 	}
 
